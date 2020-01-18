8bf95be0affd5f578eaf4eb32854d692a3049f25
mean <fixounet@free.fr>
2020-01-18 08:27:12 +0100

    GFX: Switch to Adafruit ST7735

--- tft_espi.cmake
+++
@@ -1,9 +0,0 @@
-include_directories(${CURRENT_CMAKE_SOURCE})
-set(TFT_eSPI_src TFT_eSPI/TFT_eSPI.cpp)
-generate_arduino_library(TFT_eSPI 
-                        SRCS ${TFT_eSPI_src}
-                        BOARD_CPU ${ARDUINO_CPU}
-                        )
-
-include_directories(TFT_eSPI)
-
