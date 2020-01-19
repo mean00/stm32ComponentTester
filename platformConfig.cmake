7539100e20ea9f88f36ba76cddf3d231aebcff5c
mean <fixounet@free.fr>
2020-01-19 12:20:39 +0100

    Add Arduino_stm32/Roger clarck as external submodule, a bit overkill but hey...

--- platformConfig.cmake
+++ platformConfig.cmake
@@ -1,3 +1,3 @@
-SET(PLATFORM_PATH "/home/fx/Arduino/hardware/stm32/STM32F1/")
+SET(PLATFORM_PATH "${CMAKE_CURRENT_SOURCE_DIR}/Arduino_STM32/STM32F1/")
 SET(ARDUINO_USE_NEWLIB 1)
 SET(PLATFORM_TOOLCHAIN_PATH "/home/fx/Arduino_stm32/arm-none-eabi-gcc/download/gcc-arm-none-eabi-8.2.1-1.7/bin")
