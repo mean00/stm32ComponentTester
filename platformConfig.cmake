b86e0548525f4b02c7a3d6da9e155e257f895dcc
mean <fixounet@free.fr>
2020-03-08 18:14:08 +0100

    tweak, continued

--- platformConfig.cmake
+++ platformConfig.cmake
@@ -3,10 +3,10 @@ SET(PLATFORM_PATH "${CMAKE_CURRENT_SOURCE_DIR}/Arduino_STM32/STM32F1/")
 SET(ARDUINO_UPLOAD_METHOD BMP) # Use blackmagic link, no bootloader
 MESSAGE(STATUS "HOST SYSTEM ${CMAKE_HOST_SYSTEM_NAME} ")
 # Warning the paths are mingw i.e. c:\dev becomes /c/dev
-IF("${CMAKE_HOST_SYSTEM_NAME}" STREQUAL "Windows")
-    SET(ARDUINO_SDK_PATH "/c/dev/Arduino")
-    SET(PLATFORM_TOOLCHAIN_PATH  "/c/dev/arm83/bin")
-ELSE()
+IF("${CMAKE_HOST_SYSTEM_NAME}" STREQUAL "Linux")
     # ARDUINO_SDK_PATH should be autodetected
     SET(PLATFORM_TOOLCHAIN_PATH "/home/fx/Arduino_stm32/arm-none-eabi-gcc/download/gcc-arm-none-eabi-8.2.1-1.7/bin")
+ELSE()
+    SET(ARDUINO_SDK_PATH "/c/dev/Arduino")
+    SET(PLATFORM_TOOLCHAIN_PATH  "/c/dev/arm83/bin")
 ENDIF()
