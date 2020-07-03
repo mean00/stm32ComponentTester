159416b4c3395f168092481371ef9dad1d72515e
mean <fixounet@free.fr>
2020-07-03 07:50:46 +0200

    re-enable normal mode

--- myCode.cpp
+++ myCode.cpp
@@ -174,13 +174,16 @@ void MainTask::run()
     adc->prepareDMASampling(ADC_SMPR_239_5,DSOADC::ADC_PRESCALER_8);    
     adc->stopDmaCapture();
     
-#if 0    
+#if 1    
     TesterGfx::clear();    
     
-    //testDualDma();
-    //testMonoTime();
-    //testDualTime();
-    //testDualTime();
+    testDualDma();
+    TesterControl::waitForAnyEvent();
+    testMonoTime();
+    TesterControl::waitForAnyEvent();
+    testDualTime();
+    TesterControl::waitForAnyEvent();
+    testDualTime();
     while(1)
     {
         
@@ -226,10 +229,144 @@ void mySetup(void)
   MainTask *mainTask=new MainTask();
   vTaskStartScheduler();        
 }
+
+/**
+ * \fn probe for 3 poles 
+ * @param a
+ * @param b
+ * @param c
+ * @param comp
+ */
+bool probeMe3(TestPin &a,TestPin &b,TestPin &c,Component **comp)
+{
+    COMPONENT_TYPE xtype;
+    Component *c2=Component::identity3(a,b,c,xtype);
+    if(!c2)
+        return false;
+  
+    delete *comp;
+    *comp=c2;
+    return true;
+}
+/**
+ * \fn probe for 2 poles
+ * @param a
+ * @param b
+ * @param c
+ * @param comp
+ */
+void probeMe2(TestPin &a,TestPin &b,TestPin &c,Component **comp)
+{
+    COMPONENT_TYPE xtype;
+    Component *c2=Component::identity2(a,b,c,xtype);
+    if(!c2)
+        return;
+    
+    if(!*comp) 
+    {
+        *comp=c2;
+        return ;
+    }        
+    bool replace=false;
+    if(c2->likely()>(*comp)->likely())
+    {
+        replace=true;
+    }
+    if(replace)
+    {
+        delete *comp;
+        *comp=c2;
+    }else
+    {
+        delete c2;
+    } 
+}
+
+
 /**
  * 
  */
 void myLoop(void)
 {
-  
+    COMPONENT_TYPE type;
+    
+    // 1 : Zeroing
+    TesterGfx::clear();
+    TesterGfx::print(30,40,"Zeroing..");
+    zeroAllPins();
+    TesterGfx::clear();
+    TesterGfx::print(0,40,"Click to probe");
+next:
+    TesterGfx::clear();
+    TesterGfx::print(0,40,"Detecting");
+    
+#if 1      
+    Component *c=NULL;
+
+#define PROBEME3(column, alpha, beta,gamma)     {TesterGfx::print(column,75,"x");   probeMe3(alpha,beta,gamma,&c);}     
+#define PROBEME(column, alpha, beta,gamma)     {TesterGfx::print(column,75,"*");    probeMe2(alpha,beta,gamma,&c);} 
+    
+    PROBEME3(20,pin1,pin2,pin3)
+    PROBEME3(40,pin1,pin3,pin2)
+    PROBEME3(60,pin2,pin3,pin1)
+    
+    if(!c)
+    {
+        PROBEME(20,pin1,pin2,pin3)
+        PROBEME(40,pin1,pin3,pin2)
+        PROBEME(60,pin2,pin3,pin1)
+        
+    }
+    
+    
+#else
+    TesterGfx::printStatus("Probing");
+    Component *c=new NPNBjt(pin2,pin3,pin1);
+    //Component *c=new Coil(pin1,pin2,pin3);
+    //Component *c=new Capacitor(pin3,pin2,pin1);
+#endif
+    if(!c)
+    {
+        TesterGfx::clear();
+        TesterGfx::print(0,60,"Found nothing");
+        if(TesterControl::waitForEvent() & CONTROL_LONG)
+            menuSystem();
+        return;
+    }
+    TesterGfx::clear();
+    const char *sname=c->getShortName();
+    TesterGfx::print(8,40,sname);
+    TesterGfx::print(0,60,"  Measuring");
+
+    // Valid component detected ?
+    if(c->compute())
+    {   
+        zeroAllPins();
+        TesterGfx::clear();
+        c->draw(0);     
+        while(1)
+        {
+            int evt=TesterControl::waitForEvent();            
+            if(evt & CONTROL_LONG) menuSystem(); // probe next
+            if(evt & CONTROL_SHORT) goto next; // probe next
+            // TODO LONG => menu
+            if(evt & CONTROL_ROTARY)
+            {
+                int count=TesterControl::getRotary();
+                c->changePage(count);
+            }
+        }
+
+        
+    }
+    delete c;
+    TesterGfx::printStatus("-------------");
+    for(int i=0;i<5;i++)
+    {
+        digitalWrite(LED,HIGH);
+        xDelay(200);
+        digitalWrite(LED,LOW);
+        xDelay(200);
+    }
 }
+// EOF
