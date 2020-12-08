2d237043fa302664b484d7480ac5d564543e7d98
mean <fixounet@free.fr>
2020-12-08 07:57:12 +0100

    add helper font generation

---
+++ assets/Fonts/fontgen.sh
@@ -0,0 +1,7 @@
+export FNT=LiberationSans-Regular.ttf
+export OUT=liberation
+./fontconvert $FNT 7 > ${OUT}7.h
+./fontconvert $FNT 9 > ${OUT}9.h
+./fontconvert $FNT 6 > ${OUT}6.h
+./fontconvert $FNT 11 > ${OUT}11.h
+
